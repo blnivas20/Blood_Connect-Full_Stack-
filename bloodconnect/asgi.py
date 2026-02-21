bfd6569b1d221b8c3d7e60f21d233869ebae50ff
jayanth <nallagatlajayanthkumar@gmail.com>
2026-02-21 21:55:24 +0530

    fix deploy bug

--- bloodconnect/asgi.py
+++ bloodconnect/asgi.py
@@ -1,10 +1,10 @@
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bloodconnect.settings")
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chat.routing
 from chat.jwt_middleware import JWTAuthMiddleware
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bloodconnect.settings")
 
 django_asgi_app = get_asgi_application()
 
