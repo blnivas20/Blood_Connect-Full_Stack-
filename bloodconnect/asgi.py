ad9fbcac0f1e9c040b8b3c8b89c3d886e1c33940
jayanth <nallagatlajayanthkumar@gmail.com>
2026-02-21 22:13:51 +0530

    fix it

--- bloodconnect/asgi.py
+++ bloodconnect/asgi.py
@@ -1,12 +1,12 @@
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bloodconnect.settings")
+django_asgi_app = get_asgi_application()
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chat.routing
 from chat.jwt_middleware import JWTAuthMiddleware
 
 
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
